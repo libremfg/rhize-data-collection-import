b388293f47ed91c3b71a5c98b9355ea737b361f7
Tom Hollingworth <tom@libremfg.com>
2025-10-02 12:56:08 -0400

    Add GetUnitOfMeasure

--- types/unitOfMeasure.go
+++ types/unitOfMeasure.go
@@ -9,6 +9,35 @@ import (
 	"github.com/hasura/go-graphql-client"
 )
 
+func GetUnitOfMeasure(ctx context.Context, client *graphql.Client, id string) (*domain.UnitOfMeasure, error) {
+	var query struct {
+		GetUnitOfMeasure struct {
+			ID       string           `graphql:"id"`
+			DataType *domain.DataType `graphql:"dataType"`
+		} `graphql:"getUnitOfMeasure(id: $id)"`
+	}
+
+	variables := map[string]interface{}{
+		"id": id,
+	}
+
+	bytes, err := client.QueryRaw(ctx, &query, variables)
+	if err != nil {
+		return nil, err
+	}
+
+	var response struct {
+		GetUnitOfMeasure *domain.UnitOfMeasure `json:"getUnitOfMeasure"`
+	}
+
+	err = json.Unmarshal(bytes, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.GetUnitOfMeasure, nil
+}
+
 func CreateUnitOfMeasure(ctx context.Context, client *graphql.Client, input []domain.AddUnitOfMeasureInput) error {
 	var m struct {
 		AddUnitOfMeasure struct {
