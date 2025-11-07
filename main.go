6bf26d29a128ff901aa6cdc72536be8ccfcda59f
Jarod Wilson <jarod.wilson@rhize.com>
2025-11-07 11:12:07 -0800

    chore: update README changelog and version

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const versionString = "rhize-data-collection-import v1.8.2"
+const versionString = "rhize-data-collection-import v1.8.3"
 
 func main() {
 	log.Println(versionString)
